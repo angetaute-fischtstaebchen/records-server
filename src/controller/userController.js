a2fb89d71da8520175a26ce66e9e513f82a9a941
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-22 10:16:44 +0100

    feat: error response for not id found on updateUser

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -28,10 +28,16 @@ const addUser = async (req, res, next) => {
 const updateUser = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
-      new: true,
-      useFindAndModify: false,
-    });
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      req.body,
+      {
+        new: true,
+        useFindAndModify: false,
+      },
+      (err, model) => next(err)
+    );
+
     res.json(updatedUser);
   } catch (err) {
     next(err);
